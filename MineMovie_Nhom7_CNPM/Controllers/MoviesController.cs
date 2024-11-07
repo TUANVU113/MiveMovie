726603df692c8313dc82a91b39dc4f87ec013a95
Ducy277 <trananhduc6996@gmail.com>
2024-11-07 21:07:44 +0700

    Them chuc nang

---
+++ MineMovie_Nhom7_CNPM/Controllers/MoviesController.cs
@@ -0,0 +1,52 @@
+﻿using MineMovie_Nhom7_CNPM.Models;
+using MineMovie_Nhom7_CNPM.Services;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MineMovie_Nhom7_CNPM.Controllers
+{
+    public class MoviesController : Controller
+    {
+        //Index
+        public ActionResult Index(string search = "")
+        {
+            MINEMOVIEEntities db = new MINEMOVIEEntities();
+            //Sử dụng procedure sp_TimKiemPhim để có thể tìm từ khoá tên phim tiếng Việt hiệu quả hơn
+            List<sp_TimKiemPhim_Result> dsphim = db.sp_TimKiemPhim(search).ToList();
+
+            if (dsphim == null || !dsphim.Any())
+            {
+                ViewBag.Message = "Không tìm thấy phim nào.";
+            }
+
+            ViewBag.Search = search;
+            return View(dsphim);
+        }
+
+        private readonly MovieService _movieService;
+        public MoviesController() { _movieService = new MovieService(); }
+        public async Task<ActionResult>  Details(int id)
+        {
+            MINEMOVIEEntities db = new MINEMOVIEEntities();
+            CHI_TIET_PHIM p = db.CHI_TIET_PHIM.Where(row => row.ID_PHIM == id).FirstOrDefault();
+
+            var movieDetails = await _movieService.GetMovieDetailsAsync(p.IMDB_ID);
+
+            //Cụm code ở dưới lần lượt để truy xuất rating của imdb, rotten tomatoes và metacritic -> Đưa vào viewbag -> view Details để hiển thị
+            var imdbRating = movieDetails.Ratings?.FirstOrDefault(r => r.Source == "Internet Movie Database")?.Value ?? "Không có thông tin";
+            var rtRating = movieDetails.Ratings?.FirstOrDefault(r => r.Source == "Rotten Tomatoes")?.Value ?? "Không có thông tin";
+            var metacriticRating = movieDetails.Ratings?.FirstOrDefault(r => r.Source == "Metacritic")?.Value ?? "Không có thông tin";
+            ViewBag.ImdbRating = imdbRating;
+            ViewBag.RtRating = rtRating;
+            ViewBag.MetacriticRating = metacriticRating;
+
+            return View(p);
+        }
+    }
+}
---
+++ MineMovie_Nhom7_CNPM/Services/OMDB_API.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace MineMovie_Nhom7_CNPM.Services
+{
+    public class MovieService
+    {
+        private readonly string apiKey = "696c897d"; //API key, cái này có thể lên omdb và gửi yêu cầu tạo API key khác, mỗi key được tối đa 1000 request thông tin
+        private readonly string baseUrl = "http://www.omdbapi.com/";
+
+        public async Task<MovieDetails> GetMovieDetailsAsync(string imdbid) //Hàm dùng để lấy thông tin phim từ mã của phim trên imdb
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetStringAsync($"{baseUrl}?i={imdbid}&apikey={apiKey}");
+                var movieDetails = JsonConvert.DeserializeObject<MovieDetails>(response);
+                return movieDetails;
+            }
+        }
+    }
+
+    public class MovieDetails
+    {
+        //public string Title { get; set; }
+        //public string Year { get; set; }
+        //public string Rated { get; set; }
+        //public string Released { get; set; }
+        //public string Runtime { get; set; }
+        //public string Genre { get; set; }
+        //public string Director { get; set; }
+        //public string Writer { get; set; }
+        //public string Actors { get; set; }
+        //public string Plot { get; set; }
+        //public string Language { get; set; }
+        //public string Country { get; set; }
+        //public string Awards { get; set; }
+        //public string Poster { get; set; }
+        public Ratings[] Ratings { get; set; } //có nhiều thuộc tính nhưng mà hiện tại chỉ lấy rating thôi
+    }
+
+    public class Ratings
+    {
+        public string Source { get; set; }
+        public string Value { get; set; }
+    }
+}
